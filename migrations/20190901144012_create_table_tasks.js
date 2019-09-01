38c689e6096a1d8db6d0de1ade5c1b6cfe110934
daniel.zini@hotmail.com <daniel.zini@hotmail.com>
2019-09-01 20:06:31 -0300

    Primeiro commit

---
+++ migrations/20190901144012_create_table_tasks.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('tasks', table => {
+        table.increments('id').primary();
+        table.string('desc').notNull();
+        table.dateTime('estimateAt');
+        table.dateTime('doneAt');
+        table.integer('userId').references('id').inTable('users').notNull();
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable('tasks');
+};
